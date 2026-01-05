2d8a5ff3dc038ad83b452282b963b7da2b0c1232
DoTheBestMayB <send1684@gmail.com>
2026-01-05 15:28:34 +0900

    refactor: 딥링크 클릭시 별도의 task가 아닌 동일 task로 이동하도록 수정

--- composeApp/src/androidMain/kotlin/com/dothebestmayb/dodotalk/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/dothebestmayb/dodotalk/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dothebestmayb.dodotalk
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,8 +8,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.dothebestmayb.dodotalk.navigation.ExternalUriHandler
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         var shouldShowSplashScreen = true
 
@@ -24,6 +27,14 @@ class MainActivity : ComponentActivity() {
                     shouldShowSplashScreen = false
                 }
             )
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        intent.dataString?.let {
+            ExternalUriHandler.onNewUri(it)
         }
     }
 }
